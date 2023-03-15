cc4a94d307e9e42f5ca4498f98e204873e95a573
pawanperennial21 <pawan.patidar@perennialsys.com>
2023-03-15 09:27:33 +0530

    feat: basic folder structure and routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import Registration from "./components/registration";
+import Dashboard from "./components/dashboard";
+import AuthLayout from "./layout/authLayout";
 function App() {
-  return <div className="App">Micro bank</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
